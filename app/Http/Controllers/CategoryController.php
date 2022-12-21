c2dafc386df3911750ffd1f15aa5394bfb88a39d
fakhryfernanda <mff023@gmail.com>
2022-12-21 14:18:02 +0700

    Membuat CRUD untuk tabel category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    function index()
+    {
+        $categories = Category::all();
+        return response()->json([
+            "status" => true,
+            "message" => "",
+            "data" => $categories
+        ]);
+    }
+
+    function detail($id)
+    {
+        $category = Category::find($id);
+
+        if (!isset($category)) {
+            return response()->json([
+                "status" => false,
+                "message" => "Kategori tidak ditemukan",
+                "data" => null
+            ]);
+        }
+
+        return response()->json([
+            "status" => true,
+            "message" => "",
+            "data" => $category
+        ]);
+    }
+
+    function store(Request $request)
+    {
+        $payload = $request->all();
+        if (!isset($payload["name"])) {
+            return response()->json([
+                "status" => false,
+                "message" => "Nama tidak boleh kosong",
+                "data" => null
+            ]);
+        }
+
+        $category = Category::create($payload);
+        return response()->json([
+            "status" => true,
+            "message" => "Kategori berhasil ditambahkan",
+            "data" => $category->makeHidden([
+                "id",
+                "created_at",
+                "updated_at"
+            ])
+        ]);
+    }
+
+    function edit(Request $request, $id){
+
+        $category = Category::find($id);
+        
+        if (!isset($category)) {
+            return response()->json([
+                "status" => false,
+                "message" => "Kategori tidak ditemukan",
+                "data" => null
+            ]);
+        }
+
+        $category->update($request->all());
+
+        return response()->json([
+            "status" => true,
+            "message" => "Data kategori berhasil diubah",
+            "data" => $category
+        ]);
+    }
+
+    function delete($id){
+        $category = Category::find($id);
+
+        if (!isset($category)) {
+            return response()->json([
+                "status" => false,
+                "message" => "Kategori tidak ditemukan",
+                "data" => null
+            ]);
+        }
+
+        Category::destroy($id);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Kategori berhasil dihapus"
+        ]);
+    }
 }
